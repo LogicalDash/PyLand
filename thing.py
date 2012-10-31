6d53acc943cb7dc47ff59a4dccebf3da7e09daf6
LogicalDash <LogicalDash@gmail.com>
2012-10-31 09:29:41 -0400

    Trying to move as much of the database logic into sqlite as feasible.

--- thing.py
+++ thing.py
@@ -2,13 +2,7 @@ import place
 import level
 
 class Thing:
-    loc = None
-    def __init__(self, name, desc='', loc=None, attributes=[]):
-        self.name = name
-        self.desc = desc
+    def __init__(self, loc, attributes):
+        self.loc = loc
         self.attributes = attributes
-        if loc is not None:
-            if issubclass(loc, place.Place):
-                self.loc = loc
-            elif type(loc) is str:
-                self.loc = level.getLevel().places[loc]
+        
