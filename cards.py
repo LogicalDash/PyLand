421849eee31975c18ce945c1a37645f931577337
LogicalDash <LogicalDash@gmail.com>
2013-03-11 15:08:07 -0400

    slightly less epic docstring

---
+++ cards.py
@@ -0,0 +1,22 @@
+class EventCard:
+    """A card representing a kind of thing that can happen.
+
+EventCards are kept in EventDecks, which are in turn contained by
+Characters. When something happens involving one or more characters,
+the relevant EventDecks from the participating Characters will be put
+together into an AttemptDeck. One card will be drawn from this, called
+the attempt card. It will identify what kind of EventDeck should be
+taken from the participants and compiled in the same manner into an
+OutcomeDeck. From this, the outcome card will be drawn.
+
+The effects of an event are determined by both the attempt card and
+the outcome card. An attempt card might specify that only favorable
+outcomes should be put into the OutcomeDeck; the attempt card might
+therefore count itself for a success card. But further, success cards
+may have their own effects irrespective of what particular successful
+outcome occurs. This may be used, for instance, to model that kind of
+success that strains a person terribly and causes them injury.
+
+"""
+    def __init__(self):
+        pass
